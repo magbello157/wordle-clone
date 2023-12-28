ebfa0e744f3ad617c8dca53e126f1dca48be5691
Muhammad A.G. Bello <magbello157@gmail.com>
2023-12-28 06:23:03 +0100

    add defined guess slots

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 import GuessInput from '../GuessInput';
 import GuessResults from '../GuessResults';
@@ -15,6 +16,11 @@ function Game() {
   const [guesses, setGuesses] = React.useState([]);
 
   function handleSubmitGuess(tentativeGuess) {
+    if (guesses.length === NUM_OF_GUESSES_ALLOWED) {
+      alert('Maximum tries reached!');
+      return;
+    }
+
     setGuesses([...guesses, tentativeGuess]);
   }
 
