c040d1e25a941229d57b5afb9c4843e3d71b19a1
Muhammad A.G. Bello <magbello157@gmail.com>
2023-12-28 10:23:41 +0100

    style answer cells based on result

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
 
-const Guess = ({ value }) => {
+const Cell = ({ letter, status }) => {
+  const className = status ? `cell ${status}` : 'cell';
+
+  return <span className={className}>{letter}</span>;
+};
+
+const Guess = ({ value, answer }) => {
+  const result = checkGuess(value, answer);
+
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
+        <Cell
+          key={num}
+          letter={result ? result[num].letter : undefined}
+          status={result ? result[num].status : undefined}
+        >
           {value ? value[num] : null}
-        </span>
+        </Cell>
       ))}
     </p>
   );
